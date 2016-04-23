aefae4ff1964e934a753ef8d6ad1c3e3b83ae2e6
yama@muteki <yama@muteki.com>
2016-04-23 22:50:28 +0900

    Change JS to JSON

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -5,5 +5,10 @@ class BusStopsController < ApplicationController
 
   def show
     @bus_stop = BusStop.preload(:bus_route_infos).find(params[:id])
+    @hash = Gmaps4rails.build_markers(@bus_stop) do |bus_stop, marker|
+      marker.lat bus_stop.latitude
+      marker.lng bus_stop.longitude
+      marker.infowindow bus_stop.name
+    end
   end
 end
