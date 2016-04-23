90209fe8152cc83b98ddab4052eb8b6acf367ae2
yama@muteki <yama@muteki.com>
2016-04-23 23:28:33 +0900

    Add bus route maps

--- app/controllers/bus_route_infos_controller.rb
+++ app/controllers/bus_route_infos_controller.rb
@@ -1,5 +1,10 @@
 class BusRouteInfosController < ApplicationController
   def show
     @bus_route_info = BusRouteInfo.preload(:bus_stops).find(params[:id])
+    @hash = Gmaps4rails.build_markers(@bus_route_info.bus_stops) do |bus_stop, marker|
+      marker.lat bus_stop.latitude
+      marker.lng bus_stop.longitude
+      marker.infowindow bus_stop.name
+    end
   end
 end
