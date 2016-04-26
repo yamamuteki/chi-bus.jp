daacf9cf101b251e38869726d7ea9d178b6a9969
yama@muteki <yama@muteki.com>
2016-04-27 02:44:18 +0900

    Fix remeins of keywords into fragment cache

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -1,13 +1,13 @@
 class BusStopsController < ApplicationController
   def index
-    session[:q] = params[:q]
-    @bus_stops = []
     if params[:q] then
       @bus_stops = BusStop.where("name like '%#{params[:q]}%'").order('name, latitude DESC').limit(100)
     elsif params[:position]
       latitude = params[:position].split(',')[0].to_f
       longitude = params[:position].split(',')[1].to_f
       @bus_stops = BusStop.near([latitude, longitude], 20000).limit(25)
+    else
+      @bus_stops = []
     end
   end
 
