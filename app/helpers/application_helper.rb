b17c8d4c60d8cf8e71d55dd62705e00896b2388a
yama@muteki <yama@muteki.com>
2016-04-29 00:17:33 +0900

    Show current position when to use HTML5 geolocation API

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,16 @@
 module ApplicationHelper
-  def build_markers(bus_stops)
+  def build_markers(bus_stops, position = nil)
     hash = Gmaps4rails.build_markers(bus_stops) do |bus_stop, marker|
       marker.lat bus_stop.latitude
       marker.lng bus_stop.longitude
       marker.infowindow render partial: 'infowindow', locals: { bus_stop: bus_stop }
+    end
+    if position then
+      hash + Gmaps4rails.build_markers([position]) do |bus_stop, marker|
+        marker.lat position.split(',')[0]
+        marker.lng position.split(',')[1]
+        marker.picture({ url: image_path('bluedot.png'), width: '34', height: '34' })
+      end
     end
   end
   def build_routes_hash(bus_route_infos)
