088f634edaa70ac929454b21f88df0b822050031
yama@muteki <yama@muteki.com>
2016-05-05 23:38:25 +0900

    Add place address in infowindows

--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -3,6 +3,11 @@ class BusStop < ActiveRecord::Base
   has_many :bus_routes, -> { order('operation_company, line_name') }, through: :bus_route_bus_stops
   reverse_geocoded_by :latitude, :longitude
   def address
+    # お試し実装。
     Geocoder.address("#{self.latitude},#{self.longitude}").to_s
+  end
+  def formatted_address
+    # DBにキャッシュしたら返す。
+    ''
   end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place
   extend Forwardable
 
-  def_delegators :@spot, :name
+  def_delegators :@spot, :name, :formatted_address
   def_delegator :@spot, :place_id, :id
   def_delegator :@spot, :lat, :latitude
   def_delegator :@spot, :lng, :longitude
