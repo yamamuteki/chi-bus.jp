203043fe37fafb1f9c9a6345ee470fb3040eede5
yama@muteki <yama@muteki.com>
2016-05-14 01:16:57 +0900

    Update sitemap xml

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,14 +2,14 @@
 SitemapGenerator::Sitemap.default_host = "https://www.chi-bus.jp"
 
 SitemapGenerator::Sitemap.create do
-  add root_path, :priority => 0.7, :changefreq => 'weekly'
-  add about_path, :priority => 0.7, :changefreq => 'weekly'
+  add root_path, priority: 0.7, changefreq: 'monthly'
+  add about_path, priority: 0.7, changefreq: 'monthly'
 
   BusStop.find_each do |bus_stop|
-    add bus_stop_path(bus_stop), lastmod: bus_stop.updated_at
+    add bus_stop_path(bus_stop), lastmod: bus_stop.updated_at, changefreq: 'never'
   end
 
   BusRoute.find_each do |bus_route|
-    add bus_route_path(bus_route), lastmod: bus_route.updated_at
+    add bus_route_path(bus_route), lastmod: bus_route.updated_at, changefreq: 'never'
   end
 end
