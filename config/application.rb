21911200340edc8defb072eef2fb7d3a97d3b75e
yama@muteki <yama@muteki.com>
2016-05-21 23:47:20 +0900

    Enable GC profiler

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module ChiBus
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    GC::Profiler.enable
   end
 end
