7b389de29b122c715fc70a163733c7edea50f825
yama@muteki <yama@muteki.com>
2016-06-11 21:33:14 +0900

    Fix city name

--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -2,10 +2,12 @@ class BusStop < ActiveRecord::Base
   has_many :bus_route_bus_stops
   has_many :bus_routes, -> { order('operation_company, line_name') }, through: :bus_route_bus_stops
   reverse_geocoded_by :latitude, :longitude, address: :formatted_address do |model, results|
-    if geo = results.first
+    geo = results.first
+    if geo
       model.postal_code = geo.postal_code
       # model.prefecture = geo.state # Imported db/seeds.rb
-      model.city = geo.address_components_of_type(:locality).last['long_name'] if geo.address_components_of_type(:locality).last
+      city_node = geo.address_components.select { |c| c['types'] == ['locality', 'political'] }.first
+      model.city = city_node['long_name'] if city_node
       model.formatted_address = geo.formatted_address.sub('日本, ', '')
     end
   end
