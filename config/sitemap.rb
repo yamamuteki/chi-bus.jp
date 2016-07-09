74afe70e45cac90efed12c5dd36aed73f6a2363d
yama@muteki <yama@muteki.com>
2016-07-09 13:30:59 +0900

    Change frequency to monthly in sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,15 +1,15 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://www.chi-bus.jp"
+SitemapGenerator::Sitemap.default_host = 'https://www.chi-bus.jp'
 
 SitemapGenerator::Sitemap.create do
   add root_path, priority: 0.7, changefreq: 'monthly'
   add about_path, priority: 0.7, changefreq: 'monthly'
 
   BusStop.find_each do |bus_stop|
-    add bus_stop_path(bus_stop), lastmod: bus_stop.updated_at, changefreq: 'never'
+    add bus_stop_path(bus_stop), lastmod: bus_stop.updated_at, changefreq: 'monthly'
   end
 
   BusRoute.find_each do |bus_route|
-    add bus_route_path(bus_route), lastmod: bus_route.updated_at, changefreq: 'never'
+    add bus_route_path(bus_route), lastmod: bus_route.updated_at, changefreq: 'monthly'
   end
 end
